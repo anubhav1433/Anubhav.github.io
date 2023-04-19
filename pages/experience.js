5e43a4c30b524054a10fea33448788d66bf60dcf
Anubhav Mendhiratta <anubhavmendhiratta@gmail.com>
2023-04-20 00:11:26 +0530

    Updated Experience

--- pages/experience.js
+++ pages/experience.js
@@ -15,19 +15,19 @@ export default function Experience() {
         <div data-aos="slide-up" className="text-2xl text-white hover:text-teal-500">
           <h6 className="  mr-10">☁️<span className="ml-10 text-center"> B-TECH,CSE</span></h6>
           <p className=" ml-10">PES UNIVERSITY,</p>
-          <p className="text-center lg:mr-10">Bangalore<br />2021-2025<br />CGPA: 9.21</p>
+          <p className="text-center lg:mr-16">Bangalore<br />2021-2025<br />CGPA: 9.21</p>
         </div> <br />
         {/* 2 */}
         <div data-aos="slide-up" className="text-2xl text-white hover:text-teal-500">
           <h6 className="  mr-10">☁️<span className="ml-10 text-center">12TH GRADE</span></h6>
           <p className=" ml-14">JVM SHYAMALI,</p>
-          <p className="text-center lg:mr-10">Ranchi<br />2019-2021<br />Percentage: 94.2%</p>
+          <p className="text-center lg:mr-16">Ranchi<br />2019-2021<br />Percentage: 94.2%</p>
         </div> <br />
         {/* 1 */}
         <div data-aos="slide-up" className="text-2xl text-white hover:text-teal-500">
           <h6 className="  mr-10">☁️<span className="ml-10 text-center">10TH GRADE</span></h6>
           <p className=" ml-14">ST. ANTHONY&apos;S</p>
-          <p className="text-center lg:mr-10">Ranchi<br />2007-2019<br />Percentage: 95%</p>
+          <p className="text-center lg:mr-16">Ranchi<br />2007-2019<br />Percentage: 95%</p>
         </div>
       </div >
       {/* Experience */}
@@ -35,18 +35,19 @@ export default function Experience() {
         <h4 className="fontt text-teal-500 text-center text-4xl">MY EXPERIENCE</h4><br />
         {/* 3 */}
         <div data-aos="slide-up" className="text-2xl text-white hover:text-teal-500">
-          <h6 className="  lg:mr-10">☁️<span className="ml-10 text-center">MANY MORE <span className="text-center ml-20 lg:ml-20 md:mr-20">TO COME</span></span></h6>
+          <h6 className="  lg:mr-10">☁️<span className="ml-10 text-center">MANY MORE TO</span></h6>
+          <h6 className="text-center">COME</h6>
         </div> <br /><br />
         {/* 2 */}
         <div data-aos="slide-up" className="text-2xl text-white hover:text-teal-500">
           <h6 className="  mr-10">☁️<span className="lg:ml-10 ml-2 text-center">WEB DEVELOPER</span></h6>
-          <p className=" lg:ml-14 ml-10">CodeChef PESUECC</p>
+          <p className="text-center">CodeChef PESUECC</p>
           <p className="text-center lg:mr-10">Apr&apos;23-Present</p>
         </div> <br /><br />
         {/* 1 */}
         <div data-aos="slide-up" className="text-2xl text-white hover:text-teal-500">
           <h6 className="  mr-10">☁️<span className="lg:ml-10 ml-3 text-center">WEB DEV INTERN</span></h6>
-          <p className="ml-20 lg:ml-24">CodeClause</p>
+          <p className="text-center">CodeClause</p>
           <p className="text-center">Apr&apos;23-May&apos;23</p>
         </div>
       </div >
