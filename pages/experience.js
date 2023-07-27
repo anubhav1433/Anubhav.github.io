49927c4a9f5494bd7634d7f6c9250aa2c827745d
Anubhav Mendhiratta <anubhavmendhiratta@gmail.com>
2023-07-27 15:56:55 +0530

    changed css

--- pages/experience.js
+++ pages/experience.js
@@ -9,69 +9,67 @@ export default function Experience() {
   return (<div><h3 data-aos="slide-up" className="animate-pulse text-4xl ml-20  dark:text-teal-600 md:text-center ">EXPERIENCE AND QUALIFICATIONS</h3><br /><br /><br /><br />
     <div data-aos="slide-up" className="grid lg:grid-cols-3 md:grid-cols-2 gap-11 lg:pr-20 mx-20">
       {/* Education */}
-      <div className="overflow-x-scroll from-teal-500 w-50 h-30 hover:bg-gray-900">
+      <div className=" from-teal-500 w-50 h-30 hover:bg-gray-900">
         <h4 className="fontt  text-teal-500 text-center text-4xl">MY EDUCATION</h4><br />
         {/* 3 */}
         <div data-aos="slide-up" className="text-2xl text-white hover:text-teal-500">
-          <h6 className="  mr-10">☁️<span className="ml-10 text-center"> B-TECH,CSE</span></h6>
+          <h6 className="  ">☁️<span className=" text-center"> B-TECH,CSE</span></h6>
           <p className=" ml-10">PES UNIVERSITY,</p>
-          <p className="text-center lg:mr-16">Bangalore<br />2021-2025<br />CGPA: 9.21</p>
+          <p className="ml-10">Bangalore<br />2021-2025<br />CGPA: 9.21</p>
         </div> <br />
         {/* 2 */}
         <div data-aos="slide-up" className="text-2xl text-white hover:text-teal-500">
-          <h6 className="  mr-10">☁️<span className="ml-10 text-center">12TH GRADE</span></h6>
-          <p className=" ml-14">JVM SHYAMALI,</p>
-          <p className="text-center lg:mr-16">Ranchi<br />2019-2021<br />Percentage: 94.2%</p>
+          <h6 className="  ">☁️<span className=" text-center">12TH GRADE</span></h6>
+          <p className=" ml-9">JVM SHYAMALI,</p>
+          <p className="ml-10">Ranchi<br />2019-2021<br />Percentage: 94.2%</p>
         </div> <br />
         {/* 1 */}
         <div data-aos="slide-up" className="text-2xl text-white hover:text-teal-500">
-          <h6 className="  mr-10">☁️<span className="ml-10 text-center">10TH GRADE</span></h6>
-          <p className=" ml-14">ST. ANTHONY&apos;S</p>
-          <p className="text-center lg:mr-16">Ranchi<br />2007-2019<br />Percentage: 95%</p>
+          <h6 className=" ">☁️<span className=" text-center">10TH GRADE</span></h6>
+          <p className=" ml-10">ST. ANTHONY&apos;S</p>
+          <p className="ml-10">Ranchi<br />2007-2019<br />Percentage: 95%</p>
         </div>
       </div >
       {/* Experience */}
-      <div className="overflow-x-scroll from-teal-500 w-50 h-30 hover:bg-gray-900">
+      <div className=" from-teal-500 w-50 h-30 hover:bg-gray-900">
         <h4 className="fontt text-teal-500 text-center text-4xl">MY EXPERIENCE</h4><br />
         {/* 3 */}
         <div data-aos="slide-up" className="text-2xl text-white hover:text-teal-500">
-          <h6 className="  lg:mr-10">☁️<span className="ml-10 text-center">MANY MORE TO</span></h6>
-          <h6 className="text-center">COME</h6>
+          <h6 className="  ">☁️<span className=" text-center">MANY MORE TO COME</span></h6>
         </div> <br /><br />
         {/* 2 */}
         <div data-aos="slide-up" className="text-2xl text-white hover:text-teal-500">
-          <h6 className="  mr-10">☁️<span className="lg:ml-10 ml-2 text-center">WEB DEVELOPER</span></h6>
-          <p className="text-center">CodeChef PESUECC</p>
-          <p className="text-center lg:mr-10">Apr&apos;23-Present</p>
+          <h6 className=" ">☁️<span className="text-center">WEB DEVELOPER</span></h6>
+          <p className="ml-9">CodeChef PESUECC</p>
+          <p className="ml-9">Apr&apos;23-Present</p>
         </div> <br /><br />
         {/* 1 */}
         <div data-aos="slide-up" className="text-2xl text-white hover:text-teal-500">
-          <h6 className="  mr-10">☁️<span className="lg:ml-10 ml-3 text-center">WEB DEV INTERN</span></h6>
-          <p className="text-center">CodeClause</p>
-          <p className="text-center">Apr&apos;23-May&apos;23</p>
+          <h6 className=" ">☁️<span className="text-center">WEB DEV INTERN</span></h6>
+          <p className="ml-10">CodeClause</p>
+          <p className="ml-10">Apr&apos;23-May&apos;23</p>
         </div>
       </div >
       {/* Responsibilities */}
-      <div className="overflow-x-scroll from-teal-500 w-50 h-30 hover:bg-gray-900">
-        <h4 className="fontt  text-teal-500 text-center text-3xl">MY RESPONSIBILITIES</h4><br />
+      <div className=" from-teal-500 w-50 h-30 hover:bg-gray-900">
+        <h4 className="fontt  text-teal-500 text-center text-3xl mr-5">MY RESPONSIBILITIES</h4><br />
         {/* 3 */}
         <div data-aos="slide-up" className="text-2xl text-white hover:text-teal-500">
-          <h6 className="  mr-10">☁️<span className="lg:ml-10 ml-16 text-center">HEAD OF</span></h6>
-          <h6 className="text-center">HOSPITALITY,</h6>
-          <p className=" ml-16">SHUNYA PESUECC</p>
-          <p className="text-center ">Oct&apos;22-Present</p>
+          <h6 className=" ">☁️<span className="text-center">HEAD OF HOSPITALITY,</span></h6>
+          <p className=" ml-8">SHUNYA PESUECC</p>
+          <p className="ml-8">Oct&apos;22-Present</p>
         </div> <br /> <br />
         {/* 2 */}
         <div data-aos="slide-up" className="text-2xl text-white hover:text-teal-500">
-          <h6 className="  mr-10">☁️<span className="lg:ml-10 ml-20 text-center">CORE <span className="ml-20 lg:ml-0 md:ml-20">VOLUNTEER</span></span></h6>
-          <p className=" lg:ml-24 ml-20">NGO,UPHAAR</p>
-          <p className="text-center">May&apos;20-Mar&apos;22</p>
+          <h6 className=" ">☁️<span className="text-center">CORE VOLUNTEER</span></h6>
+          <p className="ml-10">NGO,UPHAAR</p>
+          <p className="ml-10">May&apos;20-Mar&apos;22</p>
         </div> <br /> <br />
         {/* 1 */}
         <div data-aos="slide-up" className="text-2xl text-white hover:text-teal-500">
-          <h6 className="  mr-10">☁️<span className="lg:ml-12 ml-2 text-center">SPORTS CAPTAIN</span></h6>
-          <p className=" lg:ml-24 ml-14">ST. ANTHONY&apos;S</p>
-          <p className="text-center ">Jan&apos;17-Jan&apos;18</p>
+          <h6 className="">☁️<span className="text-center">SPORTS CAPTAIN</span></h6>
+          <p className=" ml-10">ST. ANTHONY&apos;S</p>
+          <p className="ml-10">Jan&apos;17-Jan&apos;18</p>
         </div>
       </div >
     </div></div>);
